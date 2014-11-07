b3eefa9a732914d578b709b1e3bc5403a803685d
schy2k <schy2k@gmail.com>
2014-11-07 13:54:06 +0900

    GIT Test
    1
    
    Signed-off-by: schy2k <schy2k@gmail.com>

--- gitTest/AppDelegate.m
+++ gitTest/AppDelegate.m
@@ -10,6 +10,7 @@
 
 @implementation AppDelegate
 
+// git test
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
